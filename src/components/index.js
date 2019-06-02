45251b203c3e567688779e2238b9cdeb7144da5d
Sinh Luu <luuvansinh555@gmail.com>
2019-06-02 22:28:28 +0700

    remove unused component

--- src/components/index.js
+++ src/components/index.js
@@ -2,16 +2,11 @@ import RcBreadcrumb from './breadcrumb'
 import RcSearch from './search'
 import RcSelectBox from './select-box'
 import RcTable from './table'
-import RcIconStatus from './icon-status'
-import RcIconVerified from './icon-verified'
 import RcSearchAutoComplete from './search-auto-complete'
 import RcDropOption from './drop-option'
 import RcDatePicker from './date-picker'
 import RcInputNumber from './input-number'
 import RcSearchSelect from './search-select'
-import RcGallery from './gallery'
-import RcDatePickerHorizontal from './date-picker-horizontal'
-import RcSelectBoxHorizontal from './select-box-horizontal'
 import ProductItem from './product-item'
 import RcSlider from './slider'
 
@@ -20,16 +15,11 @@ export {
   RcSearch,
   RcSelectBox,
   RcTable,
-  RcIconStatus,
-  RcIconVerified,
   RcSearchAutoComplete,
   RcDropOption,
   RcDatePicker,
   RcInputNumber,
   RcSearchSelect,
-  RcGallery,
-  RcDatePickerHorizontal,
-  RcSelectBoxHorizontal,
   ProductItem,
   RcSlider,
 }
